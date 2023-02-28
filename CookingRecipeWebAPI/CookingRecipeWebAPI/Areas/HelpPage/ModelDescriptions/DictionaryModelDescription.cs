f14f815cc96b5398f9ce5c4daef36508c4b61208
Kelvin-MKL <109009865+Kelvin-MKL@users.noreply.github.com>
2023-02-28 11:49:24 +1000

    project cleared

--- CookingRecipeWebAPI/CookingRecipeWebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace CookingRecipeWebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- CookingRecipeWebAPI/CookingRecipeWebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace CookingRecipeWebAPI.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
